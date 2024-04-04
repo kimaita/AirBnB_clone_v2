7afb545f8855ca332f538c57f00521b99b047199
kimaita <johnnieqaym@gmail.com>
2024-04-04 11:54:42 +0300

    codestyle fixes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from fabric.api import local
 import os
 
+
 def get_timestamp():
     """Returns the current datetime as a formatted string
 
@@ -11,6 +12,7 @@ def get_timestamp():
         str: current datetime as YYYYMMDDHHMMSS
     """
     return datetime.now().strftime('%Y%m%d%H%M%S')
+
 
 def do_pack():
     """Archives `web_static` into a tarball, saving it in `versions`
@@ -20,13 +22,13 @@ def do_pack():
         str or None: path to archive or None if archiving failed
     """
     os.makedirs('versions', exist_ok=True)
-    filename=f"web_static_{get_timestamp()}.tgz"
-    filepath=os.path.join('versions', filename)
+    filename = f"web_static_{get_timestamp()}.tgz"
+    filepath = os.path.join('versions', filename)
 
-    res=local(f"tar -cvzf {filepath} web_static")
+    res = local(f"tar -cvzf {filepath} web_static")
     if res.succeeded:
-        tarsize=os.path.getsize(filepath)
+        tarsize = os.path.getsize(filepath)
         print(f'web_static packed: {filepath} -> {tarsize}Bytes')
         return filepath
-    
+
     return None
