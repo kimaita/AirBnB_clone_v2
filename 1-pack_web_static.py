ce448dce19ef7a4d950bb33006ec8ae6216777a1
kimaita <johnnieqaym@gmail.com>
2024-04-04 11:53:20 +0300

    added archiving fabfile

---
+++ 1-pack_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""generates a .tgz archive from the contents of the web_static folder"""
+from datetime import datetime
+from fabric.api import local
+import os
+
+def get_timestamp():
+    """Returns the current datetime as a formatted string
+
+    Returns:
+        str: current datetime as YYYYMMDDHHMMSS
+    """
+    return datetime.now().strftime('%Y%m%d%H%M%S')
+
+def do_pack():
+    """Archives `web_static` into a tarball, saving it in `versions`
+
+
+    Returns:
+        str or None: path to archive or None if archiving failed
+    """
+    os.makedirs('versions', exist_ok=True)
+    filename=f"web_static_{get_timestamp()}.tgz"
+    filepath=os.path.join('versions', filename)
+
+    res=local(f"tar -cvzf {filepath} web_static")
+    if res.succeeded:
+        tarsize=os.path.getsize(filepath)
+        print(f'web_static packed: {filepath} -> {tarsize}Bytes')
+        return filepath
+    
+    return None
