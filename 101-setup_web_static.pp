c05aa2e8ad7505eb6822e4f745a4c24a95d9c515
kimaita <johnnieqaym@gmail.com>
2024-04-06 05:21:04 +0300

    added puppet file for 0-setup_web_static

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,62 @@
+# Sets up a web server for the deployment of web_static
+
+Exec { path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/' }
+
+$data_dirs = ['/data', '/data/web_static', '/data/web_static/shared'
+'/data/web_static/releases', '/data/web_static/releases/test']
+
+$curr_link = '/data/web_static/current'
+$test_file =  "${data_dirs[4]}/index.html"
+$conf_file = '/etc/nginx/sites-available/default'
+
+package { 'nginx':
+    ensure => installed
+}
+
+file { $data_dirs:
+    ensure => 'directory',
+    owner  => 'ubuntu',
+    group  => 'ubuntu'
+}
+
+file { $curr_link:
+    ensure => 'link',
+    target => $data_dirs[4]
+}
+
+$index_page = @(HOMEPAGE)
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to alx!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Hello World!</h1>
+<p>If you're seeing this something's wrong. 
+<p><em>We are using nginx.</em></p>
+</body>
+</html>
+| HOMEPAGE
+
+file { $test_file:
+    ensure  => present,
+    content => $index_page
+}
+
+location_block="location /hbnb_static {\n\t\talias ${curr_link}/;\n\t}\n"
+
+exec { 'set_route' :
+    command => sed -i "/server_name _;/a \\\t${location_block}" $conf_file
+    onlyif  => '! grep -Fq "location /hbnb_static {" $conf_file'
+}
+
+service { 'nginx' :
+    restart => 'reload'
+}
