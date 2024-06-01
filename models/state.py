adf6089a58ac77a803e946c0b05b0cbc18c099ba
kimaita <johnnieqaym@gmail.com>
2024-06-01 03:01:39 +0300

    updated project files

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""holds class State"""
+
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """Representation of state"""
+
+    if models.storage_t == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            all_cities = models.storage.all(City)
+            city_list = [c for c in all_cities.values() if c.state_id == self.id]
+            return city_list
